f2e0aecbecdb080b3543c48bea63e4948911be6c
BunnyEncoder20 <varun.verma2020@vitstudent.ac.in>
2024-08-21 10:22:27 +0530

    easily constructed an express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hellow there Node Typescript");
+import express from "express";
+import http from 'http';
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    // origin:process.env.CORS_ORIGIN,
+    credentials : true,
+}));
+
+app.use(compression())
+app.use(cookieParser())
+
+
+const server = http.createServer(app);
+const PORT = 8080;
+
+server.listen(PORT, () => {
+    console.log(`Server running at : https://localhost:${PORT}/`);
+});
